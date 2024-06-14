936be14eac0a62497b55b94bb73bd243914306c0
luis <luisjosemon@gmail.com>
2024-06-13 20:51:09 -0400

    Inicializando el proyecto Calculadora opcional terminada las fases

--- src/main/java/org/example/CalculadoraFases/CalculadoraFases.java
+++ src/main/java/org/example/CalculadoraFases/CalculadoraFases.java
@@ -2,22 +2,21 @@ package org.example.CalculadoraFases;
 
 public class CalculadoraFases {
     public int sumas(int a, int b) {
-        int suma = a + b;
-        return suma;
+        return a + b;
     }
+
     public int restas(int a, int b) {
-        int resta = a - b;
-        return resta;
+        return a - b;
     }
+
     public int multiplicaciones(int a, int b) {
-        int multiplicacion = a * b;
-        return multiplicacion;
+        return a * b;
     }
+
     public float divisiones(int a, int b) {
         if (b == 0) {
             throw new ArithmeticException("no se puede dividir por cero");
         }
-        float division =(float) a / b;
-        return division;
+        return (float) a / b;
     }
 }
